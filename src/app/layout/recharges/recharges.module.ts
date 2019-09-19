7a0fdd39a0217db267fcdbe227b2a85419eb7d40
Ousmane Ndiaye <ousmane.ndiaye6@orange-sonatel.com>
2019-09-19 20:33:19 +0000

    update page recharge

--- src/app/layout/recharges/recharges.module.ts
+++ src/app/layout/recharges/recharges.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { RechargesRoutingModule } from './recharges-routing.module';
 import { RechargesComponent } from './recharges.component';
-
+import { PageHeaderModule } from 'src/app/shared';
 
 @NgModule({
-  declarations: [RechargesComponent],
-  imports: [
-    CommonModule,
-    RechargesRoutingModule
-  ]
+	declarations: [ RechargesComponent ],
+	imports: [ CommonModule, RechargesRoutingModule, PageHeaderModule ]
 })
-export class RechargesModule { }
+export class RechargesModule {}
