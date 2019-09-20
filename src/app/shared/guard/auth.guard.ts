69190f3f22b7eca468dcca694dcb431e1e9542e0
Ousmane Ndiaye <ousmane.ndiaye6@orange-sonatel.com>
2019-09-20 23:05:35 +0000

    update module login

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -6,12 +6,15 @@ import { AuthenticationService } from '../services/authentication.service';
 export class AuthGuard implements CanActivate {
 	constructor(private router: Router, private authenticationService: AuthenticationService) {}
 
-	canActivate() {
-		if (localStorage.getItem('isLoggedin')) {
+	canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+		const currentUser = this.authenticationService.currentUserValue;
+		if (currentUser) {
+			// logged in so return true
 			return true;
 		}
 
-		this.router.navigate([ '/login' ]);
+		// not logged in so redirect to login page with the return url
+		this.router.navigate([ '/login' ], { queryParams: { returnUrl: state.url } });
 		return false;
 	}
 }
