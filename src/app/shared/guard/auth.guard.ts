ad64ca0d1530f855d616e328cbaf9e31158be305
Ousmane Ndiaye <ousmane.ndiaye6@orange-sonatel.com>
2019-09-20 21:21:05 +0000

    update module ajout carte

--- src/app/shared/guard/auth.guard.ts
+++ src/app/shared/guard/auth.guard.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
-import { Router } from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private router: Router) {}
+	constructor(private router: Router, private authenticationService: AuthenticationService) {}
 
-    canActivate() {
-        if (localStorage.getItem('isLoggedin')) {
-            return true;
-        }
+	canActivate() {
+		if (localStorage.getItem('isLoggedin')) {
+			return true;
+		}
 
-        this.router.navigate(['/login']);
-        return false;
-    }
+		this.router.navigate([ '/login' ]);
+		return false;
+	}
 }
