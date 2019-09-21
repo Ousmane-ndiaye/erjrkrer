9172de9a227a3f9f4c16670574e431eb93875a75
Ousmane Ndiaye <ousmane.ndiaye6@orange-sonatel.com>
2019-09-21 00:45:23 +0000

    desactive authentiation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,17 @@ import { AppComponent } from './app.component';
 import { AuthGuard } from './shared';
 
 const routes: Routes = [
-    { path: '', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule), canActivate: [AuthGuard] },
-    { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
-    { path: 'signup', loadChildren: () => import('./signup/signup.module').then(m => m.SignupModule) },
-    { path: 'error', loadChildren: () => import('./server-error/server-error.module').then(m => m.ServerErrorModule) },
-    { path: 'access-denied', loadChildren: () => import('./access-denied/access-denied.module').then(m => m.AccessDeniedModule) },
-    { path: 'not-found', loadChildren: () => import('./not-found/not-found.module').then(m => m.NotFoundModule) },
-    { path: '**', redirectTo: 'not-found' }
+	{ path: '', loadChildren: () => import('./layout/layout.module').then((m) => m.LayoutModule) },
+	{ path: 'login', loadChildren: () => import('./login/login.module').then((m) => m.LoginModule) },
+	{ path: 'signup', loadChildren: () => import('./signup/signup.module').then((m) => m.SignupModule) },
+	{ path: 'error', loadChildren: () => import('./server-error/server-error.module').then((m) => m.ServerErrorModule) },
+	{ path: 'access-denied', loadChildren: () => import('./access-denied/access-denied.module').then((m) => m.AccessDeniedModule) },
+	{ path: 'not-found', loadChildren: () => import('./not-found/not-found.module').then((m) => m.NotFoundModule) },
+	{ path: '**', redirectTo: 'not-found' }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
