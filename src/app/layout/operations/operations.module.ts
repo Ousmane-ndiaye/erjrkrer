a5fefbdb1a0b6f1f719deb01ff527c92b0071125
Ousmane Ndiaye <ousmane.ndiaye6@orange-sonatel.com>
2019-10-13 12:45:14 +0000

    add operations module

--- src/app/layout/operations/operations.module.ts
+++ src/app/layout/operations/operations.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { OperationsRoutingModule } from './operations-routing.module';
 import { OperationsComponent } from './operations.component';
-
+import { PageHeaderModule } from 'src/app/shared';
 
 @NgModule({
-  declarations: [OperationsComponent],
-  imports: [
-    CommonModule,
-    OperationsRoutingModule
-  ]
+	declarations: [ OperationsComponent ],
+	imports: [ CommonModule, OperationsRoutingModule, PageHeaderModule ]
 })
-export class OperationsModule { }
+export class OperationsModule {}
